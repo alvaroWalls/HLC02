efa9856e278093f4629f4d22324a428e7244cb1c
sergiorr98 <55790656+sergiorr98@users.noreply.github.com>
2019-12-17 13:41:23 +0100

    Botones de aumentar o disminuir fuente

---
+++ js/tamañoLetra.js
@@ -0,0 +1,13 @@
+var fontSize = 1;
+
+// funcion para aumentar la fuente
+function aumentarFuente() {
+    fontSize += 0.1;
+    document.body.style.fontSize = fontSize + "em";
+}
+
+// funcion para disminuir la fuente
+function disminuirFuente() {
+    fontSize -= 0.1;
+    document.body.style.fontSize = fontSize + "em";
+}
